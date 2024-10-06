935ba80de8da064d981c43005397f7d9c1f1ff0d
Maks Maltsev <malt1640@ya.ru>
2024-10-06 16:42:57 +0300

    Избавился от лишних <p>

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-// Markdown to HTML converter
-var converter = new showdown.Converter({parseImgDimensions: true});
+// https://postsandprograms.com/posts/20210722/
+showdown.extension("remove-p-from-img", function () {
+  return [
+    {
+      type: "output",
+      filter: function (text) {
+        text = text.replace(
+          // match all <p>'s before and after an <img> tag
+          /(<\/?p[^>]*>)((?=<img.+>)|(?<=<img.+>))/g,
+          ""
+        );
+        return text;
+      },
+    },
+  ];
+});
+
+// Markdown to HTML converter with an extension and a parameter
+var converter = new showdown.Converter({
+  extensions: ["remove-p-from-img"],
+  parseImgDimensions: true
+});
 
 function buildNode(nodeJSON, selectedTags, headingLevel = 2) {
   // Node body
