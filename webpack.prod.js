9b40178cd5be8b6063db677e6eaf49a20f591063
Maks Maltsev <malt1640@ya.ru>
2024-11-07 14:06:21 +0300

    CSS Extraction на проде

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -8,5 +9,18 @@ module.exports = merge(common, {
     filename: "bundle.[contenthash].js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+  },
+  plugins: [ new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }) ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader, // Extracts css into files
+          "css-loader",
+          "sass-loader",
+        ]
+      },
+    ]
   }
 });
